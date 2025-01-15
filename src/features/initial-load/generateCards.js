a99f7a6cfb45117cd934804851a97c5f8a41e422
PRISM-EXE <jwalker.swe@gmail.com>
2025-01-14 19:22:24 -0600

    Fetch of data used to generate cards now runs asynchronously allowing us to index through the array of said data

--- src/features/initial-load/generateCards.js
+++ src/features/initial-load/generateCards.js
@@ -3,36 +3,37 @@ let gallery = document.querySelector('gallery')
 
 
 class PopulateGallery {
-    constructor() {
+    constructor(totalCards) {
+        this.totalCards = totalCards;
     }
-    // Create function to generate individual cards
-    generateCards() {
-        // Set total number of cards to generate on load
-        let totalCards = 15;
-        let pokemonData = [];
+
+    // Create functions to used to generate cards
+
+    async fetchData() {
+        let results = [];
 
         // Fetch data to be used for each card
-        for(let i = 1; i < totalCards+1; i++) {
-            fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-                .then(res => res.json())
-                .then(data => {
-                    console.log(data)
-                    return pokemonData.push(data)
-                })
-                .catch(err => {
-                    console.log(`error ${err}`)
-                })
+        try {
+            for(let i = 1; i < this.totalCards+1; i++) {
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+                const data = await response.json();
+                results.push(data);
+            }
+            console.log(results);
+            return results;
+        } catch (err) {
+            console.log(`Error: ${err}`);
         }
-        console.log('Pokemon Data: ', pokemonData);
-
-        // Build out cards for each pokemon
-        pokemonData.forEach((data) => {
-            const card = document.createElement
-        })
     }
 }
 
 
-let cardInfo = new PopulateGallery;
+let generateCards = new PopulateGallery(15);
+
+generateCards.fetchData().then(results => {
+    console.log("Accessing results:", results[0]);
+}).catch(err => {
+    console.log('error');
+})
 
 //window.addEventListener('load', cardInfo.generateCards());
