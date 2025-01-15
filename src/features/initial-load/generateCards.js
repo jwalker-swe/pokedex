598433256460028362f954f41e9d4b9136c1b61d
PRISM-EXE <jwalker.swe@gmail.com>
2025-01-15 13:03:06 -0600

    Pokemon cards generated dynamically on page load

--- src/features/initial-load/generateCards.js
+++ src/features/initial-load/generateCards.js
@@ -1,5 +1,5 @@
 let container = document.querySelector('.gallery-contaienr')
-let gallery = document.querySelector('gallery')
+let gallery = document.querySelector('.gallery')
 
 
 class PopulateGallery {
@@ -33,7 +33,7 @@ class PopulateGallery {
         let newId;
 
         let pokemon = await this.fetchData();
-        console.log(pokemon);
+        //console.log(pokemon);
 
         pokemon.forEach(element => {
             listOfNames.push(element.name);
@@ -54,18 +54,70 @@ class PopulateGallery {
 
             listOfIds.push(newId);
         });
-        console.log(listOfNames);
-        console.log(listOfIds);
+        return [listOfIds, listOfNames];
     }
 
     async createCards() {
+        let listOfPokemon = await this.returnPokemon();
+        let listOfIds = listOfPokemon[0];
+        let listOfNames = listOfPokemon[1];
 
+        //console.log('List of Pokemon and their data:', listOfPokemon);
+        //console.log('List of Pokemon Ids:', listOfIds);
+        //console.log('List of Pokemon Names:', listOfNames);
+
+        for (let i = 0; i < listOfPokemon[0].length; i++) {
+            //console.log(`Pokemon name: ${listOfNames[i]}`, '/', `Pokemon ids: ${listOfIds[i]}`)
+
+            let pokemonCard = document.createElement('article');
+            pokemonCard.classList.add('pokemon-card');
+            gallery.appendChild(pokemonCard);
+
+            let backgroundGrid = document.createElement('div');
+            backgroundGrid.classList.add('background-grid');
+            pokemonCard.appendChild(backgroundGrid);
+
+            let cardBackground001 = document.createElement('div');
+            cardBackground001.classList.add('card-background-001');
+            backgroundGrid.appendChild(cardBackground001);
+
+            let cardBackground002 = document.createElement('div');
+            cardBackground002.classList.add('card-background-002');
+            backgroundGrid.appendChild(cardBackground002);
+
+            let numberContainer = document.createElement('div');
+            numberContainer.classList.add('number-container');
+            backgroundGrid.appendChild(numberContainer);
+
+            let num = document.createElement('p');
+            num.classList.add('num');
+            num.innerHTML = `${listOfIds[i]}`;
+            numberContainer.appendChild(num);
+
+            let thumbnail = document.createElement('div');
+            thumbnail.classList.add('thumbnail');
+            backgroundGrid.appendChild(thumbnail);
+
+            let img = document.createElement('img');
+            img.src = `src/assets/imgs/sprites/generation-3/pokemon/main-sprites/emerald/${i+1}.png`;
+            thumbnail.appendChild(img);
+            
+            let nameContainer = document.createElement('div');
+            nameContainer.classList.add('name-container');
+            backgroundGrid.appendChild(nameContainer);
+
+            let name = document.createElement('h6');
+            name.classList.add('name');
+            name.innerHTML = `${listOfNames[i]}`;
+            nameContainer.appendChild(name);
+        }
     }
 }
 
 
 let generateCards = new PopulateGallery(15);
-generateCards.returnPokemon();
+
+generateCards.createCards();
 
 
 /* Test indexing through array
