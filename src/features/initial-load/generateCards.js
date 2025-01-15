57aaa337bbf6bc1196c17534d52d688f6a242802
PRISM-EXE <jwalker.swe@gmail.com>
2025-01-15 11:31:22 -0600

    Isolates names and ids from fetched data and stores them into sepearate arrays

--- src/features/initial-load/generateCards.js
+++ src/features/initial-load/generateCards.js
@@ -11,6 +11,7 @@ class PopulateGallery {
 
     async fetchData() {
         let results = [];
+        let names = []
 
         // Fetch data to be used for each card
         try {
@@ -19,21 +20,57 @@ class PopulateGallery {
                 const data = await response.json();
                 results.push(data);
             }
-            console.log(results);
+            //console.log(results);
             return results;
         } catch (err) {
             console.log(`Error: ${err}`);
         }
     }
+
+    async returnPokemon() {
+        let listOfNames = []
+        let listOfIds = []
+        let newId;
+
+        let pokemon = await this.fetchData();
+        console.log(pokemon);
+
+        pokemon.forEach(element => {
+            listOfNames.push(element.name);
+
+            let id = element.id.toString();
+            let chars = id.split('');
+            let charLength = chars.length;
+
+            if (charLength === 1) {
+                chars.unshift('0', '0');
+                newId = chars.join('');
+            } else if (charLength === 2) {
+                chars.unshift('0');
+                newId = chars.join('');
+            } else {
+                newId = chars.join('');
+            }
+
+            listOfIds.push(newId);
+        });
+        console.log(listOfNames);
+        console.log(listOfIds);
+    }
+
+    async createCards() {
+
+    }
 }
 
 
 let generateCards = new PopulateGallery(15);
+generateCards.returnPokemon();
 
+
+/* Test indexing through array
 generateCards.fetchData().then(results => {
-    console.log("Accessing results:", results[0]);
+    console.log("Accessing results:", results[0].name);
 }).catch(err => {
     console.log('error');
-})
-
-//window.addEventListener('load', cardInfo.generateCards());
+}) */
