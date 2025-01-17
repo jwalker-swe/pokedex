891c8585dbc14a0e4b44dfcf9a192a7be2215712
PRISM-EXE <jwalker.swe@gmail.com>
2025-01-16 19:15:58 -0600

    Pokemon Cards now load on scroll. Need to fix a few cards occasionally getting passed condition to stop loading cards.

--- src/features/load-on-scroll/loadAdditionalCards.js
+++ src/features/load-on-scroll/loadAdditionalCards.js
@@ -1,4 +1,5 @@
 let galleryToScroll = document.querySelector('.gallery');
+let galleryContainer = document.querySelector('.gallery-container');
 
 class LoadOnScroll {
     constructor() {
@@ -20,12 +21,12 @@ class LoadOnScroll {
 
         // Fetch data to be used for each card
         try {
-            for(let i = this.startingCard; i < this.startingCard+3; i++) {
+            for(let i = this.startingCard; i < this.startingCard+15; i++) {
                 const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
                 const data = await response.json();
                 results.push(data);
             }
-            console.log(results);
+            //console.log(results);
             return results;
         } catch (err) {
             console.log(`Error: ${err}`);
@@ -63,66 +64,131 @@ class LoadOnScroll {
     }
 
     async createCards() {
-        let listOfPokemon = await this.returnPokemon();
-        let listOfIds = listOfPokemon[0];
-        let listOfNames = listOfPokemon[1];
+        if (this.cardTotal < 387) {
+            let listOfPokemon = await this.returnPokemon();
+            let listOfIds = listOfPokemon[0];
+            let listOfNames = listOfPokemon[1];
 
-        //console.log('List of Pokemon and their data:', listOfPokemon);
-        //console.log('List of Pokemon Ids:', listOfIds);
-        //console.log('List of Pokemon Names:', listOfNames);
+            //console.log('List of Pokemon and their data:', listOfPokemon);
+            //console.log('List of Pokemon Ids:', listOfIds);
+            //console.log('List of Pokemon Names:', listOfNames);
 
-        for (let i = 0; i < listOfPokemon[0].length; i++) {
-            //console.log(`Pokemon name: ${listOfNames[i]}`, '/', `Pokemon ids: ${listOfIds[i]}`)
+            for (let i = 0; i < listOfPokemon[0].length; i++) {
+                //console.log(`Pokemon name: ${listOfNames[i]}`, '/', `Pokemon ids: ${listOfIds[i]}`)
 
-            let pokemonCard = document.createElement('article');
-            pokemonCard.classList.add('pokemon-card');
-            gallery.appendChild(pokemonCard);
+                let pokemonCard = document.createElement('article');
+                pokemonCard.classList.add('pokemon-card');
+                gallery.appendChild(pokemonCard);
 
-            let backgroundGrid = document.createElement('div');
-            backgroundGrid.classList.add('background-grid');
-            pokemonCard.appendChild(backgroundGrid);
+                let backgroundGrid = document.createElement('div');
+                backgroundGrid.classList.add('background-grid');
+                pokemonCard.appendChild(backgroundGrid);
 
-            let cardBackground001 = document.createElement('div');
-            cardBackground001.classList.add('card-background-001');
-            backgroundGrid.appendChild(cardBackground001);
+                let cardBackground001 = document.createElement('div');
+                cardBackground001.classList.add('card-background-001');
+                backgroundGrid.appendChild(cardBackground001);
 
-            let cardBackground002 = document.createElement('div');
-            cardBackground002.classList.add('card-background-002');
-            backgroundGrid.appendChild(cardBackground002);
+                let cardBackground002 = document.createElement('div');
+                cardBackground002.classList.add('card-background-002');
+                backgroundGrid.appendChild(cardBackground002);
 
-            let numberContainer = document.createElement('div');
-            numberContainer.classList.add('number-container');
-            backgroundGrid.appendChild(numberContainer);
+                let numberContainer = document.createElement('div');
+                numberContainer.classList.add('number-container');
+                backgroundGrid.appendChild(numberContainer);
 
-            let num = document.createElement('p');
-            num.classList.add('num');
-            num.innerHTML = `${listOfIds[i]}`;
-            numberContainer.appendChild(num);
+                let num = document.createElement('p');
+                num.classList.add('num');
+                num.innerHTML = `${listOfIds[i]}`;
+                numberContainer.appendChild(num);
 
-            let thumbnail = document.createElement('div');
-            thumbnail.classList.add('thumbnail');
-            backgroundGrid.appendChild(thumbnail);
+                let thumbnail = document.createElement('div');
+                thumbnail.classList.add('thumbnail');
+                backgroundGrid.appendChild(thumbnail);
 
-            let img = document.createElement('img');
-            img.src = `src/assets/imgs/sprites/generation-3/pokemon/main-sprites/emerald/${i+this.cardTotal+1}.png`;
-            thumbnail.appendChild(img);
-            
-            let nameContainer = document.createElement('div');
-            nameContainer.classList.add('name-container');
-            backgroundGrid.appendChild(nameContainer);
+                let img = document.createElement('img');
+                img.src = `src/assets/imgs/sprites/generation-3/pokemon/main-sprites/emerald/${i+this.cardTotal+1}.png`;
+                thumbnail.appendChild(img);
+                
+                let nameContainer = document.createElement('div');
+                nameContainer.classList.add('name-container');
+                backgroundGrid.appendChild(nameContainer);
 
-            let name = document.createElement('h6');
-            name.classList.add('name');
-            name.innerHTML = `${listOfNames[i]}`;
-            nameContainer.appendChild(name);
+                let name = document.createElement('h6');
+                name.classList.add('name');
+                name.innerHTML = `${listOfNames[i]}`;
+                nameContainer.appendChild(name);
+            }
         }
+    }
+
+    async getLastCardBottom() {
+        let lastCard = await gallery.lastElementChild;
+        lastCard.classList.add('lastCard');
+        let lastCardPos = await lastCard.getBoundingClientRect();
+        let lastCardBottom = await lastCardPos.bottom;
+        return lastCardBottom;
+    }
+
+    async getGalleryContainerBottom() {
+        let galleryContainerPos = await galleryContainer.getBoundingClientRect();
+        let galleryContainerBottom = galleryContainerPos.bottom;
+        return galleryContainerBottom;
+    }
+
+    async calculateDiff() {
+        let cardBottom = await this.getLastCardBottom();
+        let galleryBottom = await this.getGalleryContainerBottom();
+
+        return cardBottom - galleryBottom;
+    }
+
+    async removeLastCardClass() {
+        let lastCard = gallery.lastElementChild;
+        await lastCard.classList.remove('.lastCard');
     }
 }
 
 let getCardsToLoad = new LoadOnScroll();
 
-let loadCards = function () {
-    getCardsToLoad.createCards();
+
+
+//Functions to call on scroll
+let loadCards = async function () {
+    await getCardsToLoad.createCards();
 };
 
-gallery.addEventListener('click', loadCards);
+let removeClass = async function () {
+    await getCardsToLoad.removeLastCardClass();
+}
+
+const throttle = (fn, delay) => {
+    let time = Date.now();
+
+    return () => {
+        if ((time + delay - Date.now()) <= 0) {
+            fn();
+            time = Date.now();
+        }
+    }
+}
+
+
+
+
+
+
+
+gallery.addEventListener('scroll', throttle(async () => {
+    let count = gallery.childElementCount;
+    console.log(count);
+    
+    if (count < 387) {
+        let diff = await getCardsToLoad.calculateDiff();
+        //console.log(diff);
+
+        if (diff <= 50) {
+            await loadCards();
+            await removeClass();
+        }
+    }
+}, 100))
