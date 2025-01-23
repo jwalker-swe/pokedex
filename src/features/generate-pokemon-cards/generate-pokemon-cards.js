fde39440e063b2466c3dd7a1dee9ecca6df63137
PRISM-EXE <jwalker.swe@gmail.com>
2025-01-23 15:37:36 -0600

    Loads currect data based on current page value. Need to add ability to change current page value when click next page or previous page arrows

--- src/features/generate-pokemon-cards/generate-pokemon-cards.js
+++ src/features/generate-pokemon-cards/generate-pokemon-cards.js
@@ -1,5 +1,9 @@
-// Create array of pokemon to fetch
+// Create global variables for DOM elements
+let container = document.querySelector('.gallery-contaienr')
+let gallery = document.querySelector('.gallery')
 
+
+// Create array of pokemon to fetch
 class LoadCards{
     constructor(currentPageNum) {
         this._totalPokemon = 386;
@@ -9,49 +13,149 @@ class LoadCards{
         this._loadIteration = 0;
         this._ids = [];
         this._idsPerPage = [];
+        this._pageToLoad;
         this._idsToLoad;
+        this._results = [];
+        this._listOfIds = [];
+        this._listOfNames = [];
     }
 
     getIds() {
         for(let i = 1; i <= this._totalPokemon; i++) {
             this._ids.push(i);
         }
+        return this._ids;
     }
 
     seperateIds() {
         this.getIds();
 
         for(let i = 1; i <= this._totalPages; i++) {
-            this._idsPerPage.push([this._ids.splice(0, this._perPage)]);
+           this._idsPerPage.push([this._ids.splice(0, this._perPage)]);
         }
+
+        console.log('Ids: ', this._idsPerPage);
+        return this._idsPerPage;
     }
 
-    fetchData(offset) {
-        let results = [];
+    async fetchData(offset) {
+
         this.seperateIds();
-        
-        this._idsToLoad = this._idsPerPage[currentPage - offset];
-        console.log('Ids to Load on this page: ', this._idsToLoad);
 
-        this._idsToLoad.forEach(async page => {
-            page.forEach(async id => {
-                try {
-                    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-                    const data = await response.json();
-                    results.push(data);
-                    return results;
-                } catch (err) {
-                    console.log(`Error: ${err}`);
-                }
-            });
-        });
-        console.log(results);
+        // Fetch data to store in results
+        this._pageToLoad = this._idsPerPage[this._currentPage - offset];
+        console.log('Current page to Load: ', this._pageToLoad);
+
+        for (const page of this._pageToLoad) {
+            for (const i of page) {
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+                const data = await response.json();
+                await this._results.push(data);
+            }
+        }
+        return this._results;
+
     }
 
-    isolatePokemonInfo() {
-        this.fetchData(1);
+    async isolateInfo() {
+
+        let newId;
+        let listOfNames = [];
+        let listOfIds = [];
+        await this.fetchData(1);
+
+        this._results.forEach(pokemon => {
+            listOfNames.push(pokemon.name);
+            this._listOfIds.push(pokemon.id);
+
+            let id = pokemon.id.toString();
+            let chars = id.split('');
+            let charLength = chars.length;
+
+            if (charLength === 1) {
+                chars.unshift('0','0');
+                newId = chars.join('');
+            } else if (charLength === 2) {
+                chars.unshift('0');
+                newId = chars.join('');
+            } else {
+                newId = chars.join('');
+            }
+
+            listOfIds.push(newId);
+        });
+        return [listOfIds, listOfNames];
+    }
+
+    async createCards() {
+
+        let listOfPokemon = await this.isolateInfo();
+        let listOfIds = listOfPokemon[0];
+        let listOfNames = listOfPokemon[1];
+
+        console.log(listOfIds);
+        console.log(listOfNames);
+        console.log(this._listOfIds);
+
+        for (const [index, id] of this._listOfIds.entries()) {
+
+            console.log(listOfIds[index]);
+
+            let pokemonCard = document.createElement('article');
+            pokemonCard.classList.add('pokemon-card');
+            pokemonCard.id = `${listOfIds[index]}`;
+            gallery.appendChild(pokemonCard);
+
+            let backgroundGrid = document.createElement('div');
+            backgroundGrid.classList.add('background-grid');
+            pokemonCard.appendChild(backgroundGrid);
+
+            let cardBackground001 = document.createElement('div');
+            cardBackground001.classList.add('card-background-001');
+            backgroundGrid.appendChild(cardBackground001);
+
+            let cardBackground002 = document.createElement('div');
+            cardBackground002.classList.add('card-background-002');
+            backgroundGrid.appendChild(cardBackground002);
+
+            let numberContainer = document.createElement('div');
+            numberContainer.classList.add('number-container');
+            backgroundGrid.appendChild(numberContainer);
+
+            let num = document.createElement('p');
+            num.classList.add('num');
+            num.innerHTML = `${listOfIds[index]}`;
+            numberContainer.appendChild(num);
+
+            let thumbnail = document.createElement('div');
+            thumbnail.classList.add('thumbnail');
+            backgroundGrid.appendChild(thumbnail);
+
+            let img = document.createElement('img');
+            img.src = `src/assets/imgs/sprites/generation-3/pokemon/main-sprites/emerald/${id}.png`;
+            thumbnail.appendChild(img);
+            
+            let nameContainer = document.createElement('div');
+            nameContainer.classList.add('name-container');
+            backgroundGrid.appendChild(nameContainer);
+
+            let name = document.createElement('h6');
+            name.classList.add('name');
+            name.innerHTML = `${listOfNames[index]}`;
+            nameContainer.appendChild(name);
+
+        }
     }
 }
 
 let populateCards = new LoadCards(currentPage);
-populateCards.isolatePokemonInfo();
+
+
+populateCards.createCards();
+
+
+let nextPage = function () {
+    currentPage++;
+
+    populateCards.createCards();
+}
